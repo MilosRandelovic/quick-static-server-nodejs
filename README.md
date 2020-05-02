aabc31853249f7925c0efc0be46cf035fd6e0476
Miloš Ranđelović <milos@randelovic.me>
2020-05-02 22:49:59 +1200

    Updated dependencies, readme, and logging

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-Put all the files to be served in the /public folder.
+# Overview
 
-Run `npm start`.
+This is a quick node.js server meant to serve static (HTML & other) content from a local folder.
+
+Once started, it will automatically reload content as soon as file changes are detected.
+
+## Installation
+
+Clone the repository to a local folder, navigate there in the Terminal and execute `npm install`.
+
+## Usage
+
+Put all the files to be served in the `/public` folder, and then run `npm start`.
+
+If the content you wish to serve is in another folder/repository, you can always sync them:
+
+```bash
+#!/bin/bash
+
+cd /wherever/your/current/files/are
+rm -rf /path/to/quick-static-server-nodejs/public/{,.[!.],..?}*
+cp -R . /path/to/quick-static-server-nodejs/public/
+```
+
+The default address to serve content from is `localhost:3000`.
+
+Note that the app will by default log every single request in the console. To change this, edit the `server.js` file and comment the logging instruction under `app.all('*', ...`.
+
+## "Clean" the `/public` folder
+
+To remove all contents from the `/public` folder, you can (among other ways) run `npm run reset`.
--- server.js
+++ server.js
@@ -5,30 +5,28 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Routing handlers
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Serve static HTML as well
-app.use(express.static(path.resolve(path.join(__dirname, '/public'))));
-
 // Log all requests
-app.all('*', function (request, response, next) {
+app.all('*', (request, response, next) => {
   console.log(request.method + ': ' + request.originalUrl);
   next(); // pass control to the next handler
 });
 
-// ROUTES
+// Routing handlers
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve static HTML and other content
+app.use(express.static(path.resolve(path.join(__dirname, '/public'))));
 
 // Handle errors
-app.use(function (error, request, response, next) {
+app.use((error, request, response, next) => {
   console.error(error);
   response
     .status(error.status || 500)
     .json({ status: error.status, message: error.message });
 });
 
-app.listen(port);
-
-console.log('Server started: ' + port);
+// Start the server
+app.listen(port, () => {
+  console.log('Server started: ' + port);
+});
